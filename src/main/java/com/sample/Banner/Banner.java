8018109796d7fd4acf22c5b962e46c2be02c0776
davinsrma <106535705+davinsrma@users.noreply.github.com>
2023-03-20 23:33:39 +0530

    New banner added with file, Will execute before Class

---
+++ src/main/java/com/sample/Banner/Banner.java
@@ -0,0 +1,16 @@
+package com.sample.Banner;
+
+import java.io.*;
+
+public class Banner {
+
+    public void bannerReader() throws IOException {
+        File file = new File("./src/main/java/com/sample/Banner/Banner");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine())!=null){
+            System.out.println(line);
+
+        }
+    }
+}
